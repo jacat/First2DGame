ad98a77ee1f47a352c9ab0e0a0cd595242c0a1f2
Jacat <jallen0506@gmail.com>
2015-05-29 22:21:35 -0400

    Update Game class

--- First2DGame/src/com/jacat/First2DGame/Game.java
+++ First2DGame/src/com/jacat/First2DGame/Game.java
@@ -1,13 +1,84 @@
 package com.jacat.First2DGame;
 
-public class Game {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+
+public class Game extends Canvas implements Runnable{
+	private static final long serialVersionUID = 1L;
+
 
 	public static int width = 300;
 	public static int height = width / 16 * 9;
 	public static int scale = 3;
 
 	private Thread thread;
-	
-	
-	
+	private JFrame frame;
+	private boolean running = false;
+
+	private BufferedImage image = new BufferedImage(width,height,BufferedImage.TYPE_INT_RGB);
+	private int[] pixel = ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
+
+	public Game() {
+		Dimension size = new Dimension(width * scale, height * scale);
+		setPreferredSize(size);
+
+		frame = new JFrame();
+	}
+
+	public synchronized void start()  {
+		running = true;
+		thread = new Thread(this, "Display");
+		thread.start();
+	}
+
+	public synchronized void stop ()  {
+		try  {
+			thread.join();
+		} catch (InterruptedException e)  {
+			e.printStackTrace();
+		}
+	}
+
+	public void run() {
+		while (running) {
+			update();
+			render();
+		}
+	}
+
+	public void update() {
+
+	}
+
+	public void render() {
+		BufferStrategy bs = getBufferStrategy();
+		if (bs == null){
+			createBufferStrategy(3);
+			return;
+		}
+
+		Graphics g = bs.getDrawGraphics();
+		g.setColor(Color.BLACK);
+		g.fillRect(0,0,getWidth(),getHeight());
+		g.dispose();
+		bs.show();
+
+	}
+
+	public static void main(String[] args) {
+		Game game = new Game();
+		game.frame.setResizable(false);
+		game.frame.setTitle("First 2D Game");
+		game.frame.add(game);
+		game.frame.pack();
+		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		game.frame.setLocationRelativeTo(null);
+		game.frame.setVisible(true);
+
+		game.start();
+	}
+
 }
