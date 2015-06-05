b4834d01e58d43af2c06254f29068480179b5fa0
Jacat <jallen0506@gmail.com>
2015-06-05 15:36:48 -0400

    working out bounds

--- First2DGame/src/com/jacat/First2DGame/graphics/Screen.java
+++ First2DGame/src/com/jacat/First2DGame/graphics/Screen.java
@@ -5,7 +5,7 @@ public class Screen {
     private int width, height;
     public int[] pixels;
 
-    int time = 0;
+    int xtime = 150, ytime = 50;
     int counter = 0;
 
     public Screen(int width, int height)  {
@@ -22,12 +22,15 @@ public class Screen {
 
     public void render() {
         counter++;
-        if(counter % 100 == 0){
-            time+= 1;
-        }
+        if(counter % 80 == 0)xtime--;
+        if(counter % 80 == 0)ytime--;
+
+
         for(int y = 0; y < height; y++){
+            if (ytime < 0 || ytime >= height) break;
             for(int x = 0; x < width; x++){
-                pixels[time+time*width] = 0xff00ff;
+                if (xtime < 0 || xtime >= width) break;
+                pixels[xtime+ytime*width] = 0xff00ff;
             }
         }
     }
